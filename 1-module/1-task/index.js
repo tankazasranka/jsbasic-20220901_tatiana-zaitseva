3ed95d26c4c50bf1cde5d8dd93d901be56c646aa
Tatiana Zaitseva <tatiana.zaitseva@micepartner.ru>
2022-09-08 20:25:11 +0400

    add 1,2,3,4-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
 function factorial(n) {
-  // ваш код...
+
+  if (n == 0 || n == 1){
+    return 1;
+  }
+
+  else {
+  let n_i;
+  let factorial_n = 1;
+
+  for (let i = 0; i!=n; i++){
+    n_i=n-i;
+    factorial_n=factorial_n*n_i
+  }
+  return factorial_n;
+}
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,4 @@
 function checkSpam(str) {
-  // ваш код...
+  str=str.toUpperCase();
+  return str.includes("1xBet".toUpperCase()) || str.includes("XXX".toUpperCase());
 }

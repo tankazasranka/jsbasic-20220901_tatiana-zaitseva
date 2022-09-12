59f3ecc0ac166bcc084f83651c1592b8ce9b3f95
tankazasranka <tanka.zaitseva2016@yandex.ru>
2022-09-12 16:33:14 +0400

    add 2-module task-1,2,3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if ( Number.isFinite(salaries[key]) ) {
+      let operand_1 = salaries[key];
+      sum = operand_1 + sum;
+        }
+  }
+  return sum;
 }

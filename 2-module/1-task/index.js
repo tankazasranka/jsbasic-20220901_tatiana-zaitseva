b7fc4022daa88e53f7edad39816fd82e570b4e1a
tankazasranka <tanka.zaitseva2016@yandex.ru>
2022-09-13 09:43:51 +0400

    correct task-1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,8 +2,7 @@ function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
     if ( Number.isFinite(salaries[key]) ) {
-      let operand_1 = salaries[key];
-      sum = operand_1 + sum;
+      sum += salaries[key];
         }
   }
   return sum;

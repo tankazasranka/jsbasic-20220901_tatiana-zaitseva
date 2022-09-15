eefc6807cbc4ee54116ab2aa986e5128bb156710
tankazasranka <tanka.zaitseva2016@yandex.ru>
2022-09-15 12:09:58 +0400

    add 3-module 2,5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  arr = str.split(' ').map(item => !isNaN(item)  &&  +item ).filter(item => typeof(item) == "number").sort(compareNumeric);
+  
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+    }
+	
+  result.min = arr.shift();
+  result.max = arr.pop();
+  return result;
 }

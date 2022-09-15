ff486cb878f70c30d0400f6e87611efdea246c96
tankazasranka <tanka.zaitseva2016@yandex.ru>
2022-09-15 16:12:39 +0400

    add 3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  arr = str.split('');
+  return 	arr.map((item, index, arr) => {
+    if (item != '-') {
+      return item;
+    } 
+    arr.splice(index,1);
+    return arr[index].toUpperCase()  }).join('');
 }
